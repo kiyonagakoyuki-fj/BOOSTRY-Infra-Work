e0cd9724b8d6501318b481c74593f11ca7d7b658
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-03-19 12:34:40 +0900

    quorum-init

--- quorum/quorum-init.sh
+++ quorum/quorum-init.sh
@@ -4,20 +4,36 @@ set -e
 
 # 引数チェック
 MESSAGE='Usage: quorum-init.sh <node-label>
-    node-label: A ~ D'
+                node-label: A ~ D'
 
 if ( [ $# -ne 1 ] ); then
     echo "$MESSAGE"
     exit
 fi
 
-
-# gethdirの初期化
+# datadirの初期化
 sudo rm -rf qdata
+mkdir qdata/{logs,keys}
+mkdir qdata/dd/geth
+mkdir qdata/dd/keystore
 
-# 
+
+# 変数
 node_label="$1"
 pwd=`pwd`
 
-echo $node_label
-echo $pwd
+# 設定copy
+cp istanbul-genesis.json qdata/genesis.json
+cp static-nodes.json qdata/dd/static-nodes.json
+cp static-nodes.json qdata/dd/permissioned-nodes.json
+cp tmconf/tm${node_label}.conf qdata/tm.conf
+
+# key copy
+cp keys/nodekey${node_label} qdata/dd/geth/nodekey
+cp keys/key${node_label}.json qdata/dd/keystore/acckey
+cp keys/tm${node_label}.key qdata/keys/tm.key
+cp keys/tm${node_label}.pub qdata/keys/tm.pub
+
+# docker run時の実行sh
+cp start-node.sh qdata/start-node.sh
+
