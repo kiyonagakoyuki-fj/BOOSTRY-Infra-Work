ee1748df7371824894de79785af1fa0872b5484f
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-03-20 16:31:20 +0900

    mine

--- quorum/start-node.sh
+++ quorum/start-node.sh
@@ -17,5 +17,5 @@ nohup /usr/local/bin/constellation-node $TMCONF 2>> /qdata/logs/constellation.lo
 sleep 10
 
 echo "[*] Starting node"
-GLOBAL_ARGS='--datadir /qdata/dd --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul,rpc --permissioned --rpcport 8545 --port 21000'
+GLOBAL_ARGS='--syncmode full --mine --datadir /qdata/dd --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul,rpc --permissioned --rpcport 8545 --port 21000'
 PRIVATE_CONFIG=$TMCONF /usr/local/bin/geth $GLOBAL_ARGS --unlock 0 --password /qdata/pass.txt
