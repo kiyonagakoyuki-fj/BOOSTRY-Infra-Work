26a80878aeab092caac1e226a1081b3647520ddb
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-03-22 13:06:29 +0900

    -d削除

--- quorum/quorum-create-account.sh
+++ quorum/quorum-create-account.sh
@@ -6,4 +6,4 @@ set -e
 pwd=`pwd`
 
 # geth 起動
-docker run --rm -d --name quorum-create -v $pwd/qdata:/qdata quorum /usr/local/bin/geth account new --datadir /qdata/dd --password /qdata/pass.txt
+docker run --rm --name quorum-create -v $pwd/qdata:/qdata quorum /usr/local/bin/geth account new --datadir /qdata/dd --password /qdata/pass.txt
