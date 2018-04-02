7d28a919a8fb6907969f073e6d1fc736fb92907a
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-04-02 19:34:01 +0900

    static_node

--- installer/quorum-validator-installer.sh
+++ installer/quorum-validator-installer.sh
@@ -6,21 +6,34 @@ cd quorum
 sudo docker build -t quorum .
 
 
-# 初期
-pwd=`pwd`
+# 初期設定
 mkdir -p qdata/{logs,keys}
 mkdir -p qdata/dd/geth
 mkdir -p qdata/dd/keystore
+cp istanbul-genesis.json qdata/genesis.json
+cp static-nodes.json qdata/dd/static-nodes.json
+
+pwd=`pwd`
+STATIC_NODES=$(cat static-nodes.json)
+CURRENT_HOST_IP="10.0.0.0"
 
 # Initializing quorum
-cp istanbul-genesis.json qdata/genesis.json
+
 sudo docker run --rm -v $pwd/qdata:/qdata quorum /usr/local/bin/geth --datadir /qdata/dd init /qdata/genesis.json
 
 # Generate the node's Enode and key
 sudo docker run --rm -v $pwd/qdata:/qdata quorum /usr/local/bin/bootnode -genkey /qdata/dd/nodekey
 enode=`sudo docker run --rm -v $pwd/qdata:/qdata quorum /usr/local/bin/bootnode -nodekey /qdata/dd/nodekey -writeaddress`
+ENODE=",
+    \"$enode://${enode}@${CURRENT_HOST_IP}:21000?discport=0\"
+]"
+STATIC_NODES=${STATIC_NODES::-2}
+STATIC_NODES="$STATIC_NODES$ENODE"
+echo "$STATIC_NODES" > qdata/dd/static-nodes.json
 
-echo $enode > txt.log
+
+#enode://${enode}@${CURRENT_HOST_IP}:21000?discport=0"
+
 # 設定copy
 # cp istanbul-genesis.json qdata/genesis.json
 # cp static-nodes.json qdata/dd/static-nodes.json
