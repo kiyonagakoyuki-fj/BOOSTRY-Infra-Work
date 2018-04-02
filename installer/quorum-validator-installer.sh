0c9f3cac64e00143f4d12a1cfac172ea51228597
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-04-02 18:49:29 +0900

    enode

--- installer/quorum-validator-installer.sh
+++ installer/quorum-validator-installer.sh
@@ -17,7 +17,8 @@ cp istanbul-genesis.json qdata/genesis.json
 sudo docker run --rm -d -v $pwd/qdata:/qdata quorum /usr/local/bin/geth --datadir /qdata/dd init /qdata/genesis.json
 
 # Generate the node's Enode and key
-enode=`sudo docker run --rm -d -v $pwd/qdata:/qdata quorum /usr/local/bin/bootnode -genkey /qdata/dd/nodekey -writeaddress`
+sudo docker run --rm -d -v $pwd/qdata:/qdata quorum /usr/local/bin/bootnode -genkey /qdata/dd/nodekey
+enode=`sudo docker run --rm -d -v $pwd/qdata:/qdata quorum /usr/local/bin/bootnode -nodekey /qdata/dd/nodekey -writeaddress`
 
 echo $enode
 # 設定copy
