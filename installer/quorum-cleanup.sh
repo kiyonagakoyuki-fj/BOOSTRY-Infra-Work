d544f693e706f830e53bb3a2c27283261f7c8c51
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-04-02 18:28:47 +0900

    コンテナ存在確認

--- installer/quorum-cleanup.sh
+++ installer/quorum-cleanup.sh
@@ -2,7 +2,7 @@
 set -Ceu
 
 # docker stop
-sudo docker stop quorum
+sudo docker ps -a --filter "name=quorum" | awk 'BEGIN{i=0}{i++;}END{if(i>=2)system("sudo docker stop quorum")}'
 
 # ディレクトリ削除
 rm -rf quorum/qdata
