c0f56c31909577ddf066ea4b7dc1404df4487626
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-04-04 11:33:05 +0900

    posgres停止

--- installer/issuer-cleanup.sh
+++ installer/issuer-cleanup.sh
@@ -2,5 +2,5 @@
 set -Ceu
 
 # postgresql削除
-sudo docker stop postgres
+sudo docker ps -a --filter "name=postgres" | awk 'BEGIN{i=0}{i++;}END{if(i>=2)system("sudo docker stop postgres")}'
 sudo rm -rf ~/postgresql_data
