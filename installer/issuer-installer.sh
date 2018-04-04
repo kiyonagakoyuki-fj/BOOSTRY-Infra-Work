57fba07771b858e07d3440dca23fed11898f94f5
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-04-04 11:26:48 +0900

    it削除

--- installer/issuer-installer.sh
+++ installer/issuer-installer.sh
@@ -6,7 +6,7 @@ mkdir ~/postgresql_data
 sudo docker run --rm -d --name postgres -p 5432:5432 -v ~/postgresql_data:/var/lib/postgresql/data postgres:9.6
 
 # DB作成
-sudo docker run -it --rm --link postgres:postgres postgres:9.6 psql -h postgres -U postgres <<END
+sudo docker run --rm --link postgres:postgres postgres:9.6 psql -h postgres -U postgres <<END
 CREATE ROLE apluser LOGIN CREATEDB PASSWORD 'apluserpass';
 CREATE DATABASE apldb OWNER apluser;
 \l
