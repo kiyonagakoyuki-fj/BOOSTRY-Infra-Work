0fb61435c65fc80001b40adae28f9eb7716708e0
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-04-04 11:53:10 +0900

    createdb

--- installer/issuer-installer.sh
+++ installer/issuer-installer.sh
@@ -6,9 +6,4 @@ mkdir -p ~/postgresql_data
 sudo docker run --rm -d --name postgres -p 5432:5432 -v ~/postgresql_data:/var/lib/postgresql/data postgres:9.6
 
 # DB作成
-sudo docker run --rm --link postgres:postgres postgres:9.6 psql -h postgres -U postgres <<END
-CREATE ROLE apluser LOGIN CREATEDB PASSWORD 'apluserpass';
-CREATE DATABASE apldb OWNER apluser;
-\l
-\q
-END
+sudo docker run --rm --link postgres:postgres postgres:9.6 psql -f installer/createdb.sql -h postgres -U postgres 
