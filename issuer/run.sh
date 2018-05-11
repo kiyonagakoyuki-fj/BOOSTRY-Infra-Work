e2ed25e82f3d36714cd4cd1568130dc95ff9be20
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-05-11 15:46:52 +0900

    processor_OrderBook追加、ログを標準出力に。

--- issuer/run.sh
+++ issuer/run.sh
@@ -4,7 +4,7 @@ source ~/.bash_profile
 cd /app/tmr-issuer
 
 # async
-nohup python async/processor_IssueEvent.py &
+nohup python async/processor_IssueEvent.py < /dev/null 2>&1 /dev/null &
 
 #run server
 python manage.py runserver -h 0.0.0.0
--- node/run.sh
+++ node/run.sh
@@ -3,6 +3,8 @@ source ~/.bash_profile
 
 cd /app/tmr-node
 
+# async
+nohup python async/processor_OrderBook.py < /dev/null 2>&1 /dev/null &
+
 #run server
 ./bin/run.sh start
-
